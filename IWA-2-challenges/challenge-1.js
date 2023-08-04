bc8f7eeec4ecf2e5b19d0a874a1b842477fcdee7
Mr MN <regaltyrones@gmail.com>
2023-08-04 12:05:28 +0200

    commit unrevised challenges

---
+++ IWA-2-challenges/challenge-1.js
@@ -0,0 +1,12 @@
+/* This is the firstname of the user */
+//fixed the first comment to proper multiple line comment
+const user = 'John';
+
+
+/* This is the lastname of the user /*
+*/  //fixed the multiple-line comment just like above
+
+const surname = 'Smith';
+
+
+console.log(user, surname);     //replaced semi-colons with commas
---
+++ IWA-2-challenges/chellenge-3.js
@@ -0,0 +1,12 @@
+/* 
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/          //ammended the comments to proper multipleline comments (had / instead *)
+
+console.warn('Security scan starting'); //put string in quotes
+
+/* It is important to let user know when they can close the page  /*
+// fixed closing comment from /* to */
+cnosole.info('Please wait for scan to complete before closing the browser.');   //close quote string
