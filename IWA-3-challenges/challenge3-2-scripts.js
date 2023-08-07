902ddbde8d1bdbc4aa619ec458324d76cc00658d
Mr MN <regaltyrones@gmail.com>
2023-08-08 00:47:32 +0200

    updated comments

--- IWA-3-challenges/challenge3-2-scripts.js
+++ IWA-3-challenges/challenge3-2-scripts.js
@@ -1,4 +1,10 @@
 // scripts.js
+/*added imports of the whole files and selected the required fields to log on console*/
+
+// the imports could have been done differently by importing specifically the
+// required variable but this method is prefered in the event the variables to
+// be imported are a lot
+
 import * as nwabisa from './nwabisa.js';
 import * as johhanes from './johannes.js';
 import * as alex from './alex.js'
--- IWA-3-challenges/nwabisa.js
+++ IWA-3-challenges/nwabisa.js
@@ -1,8 +1,13 @@
-// johannes.js
-
+// johannes.js adderd 'export' function so that these variables could be
+//accessible to external js files with proper reference.
+//this could have been done at the bottom by using the ff sysntax
+//export {firstName, surname, role}
 export const firstname = "Nwabisa"
 export const surname = "Gabe"
 export let role = "CEO"
 
+//also changed the private variable data type to const
 const display= firstname + " " + surname + " (" + role + ")"
 document.querySelector('#nwabisa').innerText = display
+
+// these changes have been made to all the team mates js files
