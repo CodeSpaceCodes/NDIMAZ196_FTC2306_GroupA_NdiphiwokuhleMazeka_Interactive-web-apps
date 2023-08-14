ba54a6e7587285177a46653807a862b164356f1e
Mr MN <regaltyrones@gmail.com>
2023-08-14 15:08:39 +0200

    removed  interpolation syntax,  variables unqoute

---
+++ IWA-7/challenge2.js
@@ -0,0 +1,6 @@
+const nickname = "Timmy";
+const firstname = "Timothy";
+
+console.log("Good Morning, ", nickname || firstname)   
+// removed interpolation syntax (${})
+//took the given variables out of the quotation marks   
