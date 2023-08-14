87cc2dfc35572ce43c9f2d96f3ddfde7d49705e8
Mr MN <regaltyrones@gmail.com>
2023-08-14 16:03:37 +0200

    used backticks or interpolation

--- IWA-7/challenge2.js
+++ IWA-7/challenge2.js
@@ -1,6 +1,6 @@
-const nickname = "Timmy";
-const firstname = "Timothy";
+const nickname = null//"Timmy";
+const firstname = null//"Timothy";
+//made the printOut conditional variable to evaluate for the proper string literal to be used
+const printOut = (nickname && `, ${nickname}`) || (firstname && `, ${firstname}`) || "!"
 
-console.log("Good Morning, ", nickname || firstname)   
-// removed interpolation syntax (${})
-//took the given variables out of the quotation marks   
+console.log(`Good Morning${printOut}`)
